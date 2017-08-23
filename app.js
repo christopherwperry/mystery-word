2461f87ed257b0a9ac6dbff46ff2c2aa2e7e7ca1
christopherwperry <christopherwperry@gmail.com>
2017-08-23 15:40:16 -0400

    game logic complete, missing red display of missed letters

--- app.js
+++ app.js
@@ -7,14 +7,6 @@ const app = express();
 const fs = require('fs');
 const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
 
-let guesses = 8;
-let letters = [];
-let match;
-let wordLetters;
-let letterGuess;
-let wordArray;
-let letter;
-
 app.use(express.static(__dirname + '/public'));
 app.engine('mustache', mustache());
 app.set('views', ['./views']);
@@ -30,49 +22,77 @@ app.use(session({
 
 app.get('/', function(req, res){
   req.session.word = words[Math.floor(Math.random() * words.length)];
+  req.session.guesses = 8;
+  guesses = req.session.guesses
   wordLetters = Array.from(req.session.word);
-  guesses = 8;
+  points = 0;
+  gameStart = true;
   letters = [];
   wordArray = [];
   for (let i = 0; i < wordLetters.length; i++){
     wordArray.push("");
   }
-  res.render('index', {guesses: guesses, letters: letters, wordArray});
+  res.render('index', {guesses, letters, wordArray, gameStart});
 });
 
 app.post('/', function(req, res){
-  letterGuess = req.body.letter;
-    if (letterGuess === ""){
-      res.render('index', {guesses: guesses, letters: letters, wordArray});
+    letterGuess = req.body.letter.toLowerCase();
+    if (!letterGuess){
+      authError = "Please enter a letter."
+      res.render('index', {guesses, letters, wordArray, authError});
     } else {
-      let letterCheck = letters.indexOf(letterGuess);
-      if (letterCheck === -1){
+      if (letters.indexOf(letterGuess) === -1){
         match = false;
         checkLetter();
+        console.log(wordLetters);
+        console.log(wordArray);
         letters.push(letterGuess);
-        if (guesses === 0){
-          res.render('index');
+        if (points === req.session.word.length){
+          winner = "Winner, Winner!!";
+          newGame = true;
+          res.render('index', {winner, guesses, letters, wordArray, newGame});
+        } else if (guesses === 0){
+          authError = "You Lose!"
+          newGame = true;
+          missedLetters();
+          res.render('index', {guesses, letters, wordArray, authError, newGame});
         } else {
-          res.render('index', {guesses: guesses, letters: letters, wordArray});
+          res.render('index', {guesses, letters, wordArray});
         }
+      } else {
+        authError = "Letter already guessed. Please try another!"
+        res.render('index', {guesses, letters, wordArray, authError});
       }
     }
   });
+
+app.post('/new', function(req, res){
+  res.redirect('/')
+});
 
 function checkLetter(){
   for (let i = 0; i < wordLetters.length; i++){
     if (letterGuess === wordLetters[i]){
       match = true;
-      letter = wordLetters[i];
-      wordArray[i] = letter;
+      points+=1;
+      wordArray[i] = wordLetters[i];
     }
   };
   if (match === false){
-    guesses = guesses - 1;
+    guesses-=1;
   };
-  return wordArray
-  return guesses
+  return wordArray;
+  return guesses;
 };
+
+function missedLetters(){
+  for (let i = 0; i < wordLetters.length; i++){
+    if (wordArray[i] === ""){
+      wordArray[i] = wordLetters[i];
+    }
+  }
+  return wordArray;
+}
 
 app.listen(3000, function(){
   console.log("Server running on port 3000");
